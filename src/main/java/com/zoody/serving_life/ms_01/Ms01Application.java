582886d1c873069d513a06d4fcdc10d6b3301b26
prakhar <prakharkushwaha001@gmail.com>
2025-03-12 14:19:45 +0530

    initial state of MicroService

---
+++ src/main/java/com/zoody/serving_life/ms_01/Ms01Application.java
@@ -0,0 +1,13 @@
+package com.zoody.serving_life.ms_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoody/serving_life/ms_01/Ms01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoody.serving_life.ms_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
